bd87e890d2e1c2d6607bc397852fb64e13ef8558
PaulD95-git <r39ij6ua@students.codeinstitute.net>
2025-03-11 12:31:33 +0000

    Admin Register models created

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Table, Reservation
 
 # Register your models here.
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('number', 'capacity')
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date', 'time', 'guests', 'table', 'booking_code', 'created_at')
+    search_fields = ('name', 'email', 'phone', 'booking_code')
+    list_filter = ('date', 'time')
